19997533a08b5f864b95e9eefc3e5ba2c337de4a
TaurusPriyank <priyank.shah@thinktaurus.com>
2025-10-07 12:52:58 +0530

    Fixes

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -71,7 +71,7 @@ const UserDropdown = () => {
 
         <DropdownMenuSeparator className=" hidden sm:block bg-gray-600" />
         <nav className="sm:hidden">
-            <NavItems/>
+          <NavItems />
         </nav>
       </DropdownMenuContent>
     </DropdownMenu>
